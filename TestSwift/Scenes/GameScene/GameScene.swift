52d4435872e4dcec03380f6a4ad13e85148ced4a
RyujiOhgishi1101 <ryuuji.ohgishi@jp.ricoh.com>
2022-12-04 19:04:28 +0900

    卵とひよこが重複するバグ修正

--- TestSwift/Scenes/GameScene/GameScene.swift
+++ TestSwift/Scenes/GameScene/GameScene.swift
@@ -9,29 +9,29 @@ import SpriteKit
 
 class GameScene: SKScene {
     let sscale: Float = 0.5
+
     
-    // モグラの位置（[x,y]のセットで5個所）を作る
-    let mogPoint = [[200, 300],
-                    [600, 400],
-                    [300, 600],
-                    [650, 700],
-                    [250, 900]]
-    let images = ["attachment-5.jpg","attachment-6.jpg","attachment-8.jpg"]
-    let images2 = ["attachment-4.jpg","attachment-7.jpg","attachment-9.jpg"]
+    // 卵の位置（[x,y]のセットで5個所）を作る
+    let eggPoint = [[200, 300],[600, 400],[300, 600],[650, 700],[250, 900]]
     
-    // モグラを入れる配列を用意する（タッチしたかを調べるため）
-    var eggArray:[SKSpriteNode] = []
-    var egg2Array:[SKSpriteNode] = []
+    let egg_image = ["attachment-5.jpg","attachment-6.jpg","attachment-8.jpg"]
+    let chick_image = ["attachment-4.jpg","attachment-7.jpg","attachment-9.jpg"]
     
-    var action2_S : [SKAction] = []
-    var actionR : [SKAction] = []
+    // 卵とひよこを入れる配列を用意する（タッチしたかを調べるため）
+    var egg_nodes:[SKSpriteNode] = []
+    var chick_nodes:[SKSpriteNode] = []
+    
+    var egg_actions : [SKAction] = []
+    var chick_actions : [SKAction] = []
+    
+    var action_flag : [Bool] = []
     
     // スコアと、スコア表示用ラベルを用意する
     var score = 0
     let scoreLabel = SKLabelNode(fontNamed: "Verdana-bold")
     
     // 残り時間と、残り時間表示用ラベルと、タイマーを用意する
-    var timeCount = 20
+    var timeCount = 30
     
     let timeLabel  = SKLabelNode(fontNamed: "Verdana-bold")
     var myTimer = Timer()
@@ -55,73 +55,75 @@ class GameScene: SKScene {
                                       y: 600)
         self.addChild(scoreLabel)
         
-        // モグラの準備をする
         for i in 0...4 {
             let randomInt = Int.random(in: 0..<3)
             
             //卵の画像
-            let egg = SKSpriteNode(imageNamed: images[randomInt])
+            let egg = SKSpriteNode(imageNamed: egg_image[randomInt])
             
             egg.setScale(0.5)
-            egg.position = CGPoint(x: Int(Float(mogPoint[i][0]) * sscale),
-                                   y: Int(Float(mogPoint[i][1]) * sscale))
+            egg.position = CGPoint(x: Int(Float(eggPoint[i][0]) * sscale),
+                                   y: Int(Float(eggPoint[i][1]) * sscale))
             self.addChild(egg)
             
             //卵が割れてひよこが出てくる画像
-            let egg2 = SKSpriteNode(imageNamed: images2[randomInt])
+            let chick = SKSpriteNode(imageNamed: chick_image[randomInt])
             
-            egg2.setScale(0.5)
-            egg2.position = CGPoint(x: Int(Float(mogPoint[i][0]) * sscale),
-                                   y: Int(Float(mogPoint[i][1]) * sscale))
-            self.addChild(egg2)
+            chick.setScale(0.5)
+            chick.position = CGPoint(x: Int(Float(eggPoint[i][0]) * sscale),
+                                   y: Int(Float(eggPoint[i][1]) * sscale))
+            self.addChild(chick)
+        
+            egg_nodes.append(egg)
+            chick_nodes.append(chick)
+        
+            action_flag.append(false)
             
-            
-            eggArray.append(egg)
-            egg2Array.append(egg2)
-            // 【モグラが穴から出たり入ったりするアクションをつける】
-            // モグラを地下（-1000）に移動するアクション
-            let action1 = SKAction.moveTo(y: -1000,
+            // 卵を地下（-1000）に移動するアクション
+            let egg_action1 = SKAction.moveTo(y: -1000,
                                           duration: 0.0)
+            
             // 0〜4秒（2秒を中心として前後4秒範囲）でランダムに時間待ち
-            let action2 = SKAction.wait(forDuration: 2.0,
+            let egg_action2 = SKAction.wait(forDuration: 2.0,
                                         withRange: 4.0)
-            // もぐらを穴の位置に移動するアクション
-            let action3 = SKAction.moveTo(y: CGFloat(Int(Float(mogPoint[i][1]))),
+            // 卵を穴の位置に移動するアクション
+            let egg_action3 = SKAction.moveTo(y: CGFloat(Int(Float(eggPoint[i][1]))),
                                           duration: 0.0)
             // 0〜2秒（1秒を中心として前後2秒範囲）でランダムに時間待ち
-            let action4 = SKAction.wait(forDuration: 1.0,
+            let egg_action4 = SKAction.wait(forDuration: 1.0,
                                         withRange: 2.0)
             
-            let action5 = SKAction.moveTo(y: -1000,
+            let egg_action5 = SKAction.moveTo(y: -1000,
                                           duration: 0.0)
             
             // action1〜action4を順番に行う
-            let actionS = SKAction.sequence([action1,
-                                             action2,
-                                             action3,
-                                             action4,
-                                             action5])
-            // actionSをずっと繰り返す
-            actionR.append(SKAction.repeatForever(actionS))
+            egg_actions.append(SKAction.sequence([egg_action1,
+                                             egg_action2,
+                                             egg_action3,
+                                             egg_action4,
+                                             egg_action5]))
             
-            // モグラを地下（-1000）に移動するアクション
-            let action2_1 = SKAction.moveTo(y: -1000,
+            // ひよこを地下（-1000）に移動するアクション
+            let chick_action1 = SKAction.moveTo(y: -1000,
                                           duration: 0.0)
             
-            // もぐらを穴の位置に移動するアクション
-            let action2_2 = SKAction.moveTo(y: CGFloat(Int(Float(mogPoint[i][1]))),
+            // ひよこを穴の位置に移動するアクション
+            let chick_action2 = SKAction.moveTo(y: CGFloat(Int(Float(eggPoint[i][1]))),
                                           duration: 0.0)
             
             // 0〜4秒（2秒を中心として前後4秒範囲）でランダムに時間待ち
-            let action2_3 = SKAction.wait(forDuration: 2.0,
+            let chick_action3 = SKAction.wait(forDuration: 2.0,
                                         withRange: 4.0)
             
-            action2_S.append(SKAction.sequence([action2_1,
-                                             action2_2,
-                                             action2_3]))
+            let chick_action4 = SKAction.moveTo(y: -1000,
+                                          duration: 0.0)
+            chick_actions.append(SKAction.sequence([chick_action1,
+                                                    chick_action2,
+                                                    chick_action3,
+                                                    chick_action4]))
             
-            
-        
+            egg.run(egg_action1)
+            chick.run(chick_action4)
         }
         // 残り時間を表示する
         timeLabel.text = "Time:\(timeCount)"
@@ -131,6 +133,7 @@ class GameScene: SKScene {
         timeLabel.position = CGPoint(x: 240,
                                      y: 600)
         self.addChild(timeLabel)
+        
         // タイマーをスタートする（1.0秒ごとにtimerUpdateを繰り返し実行）
         myTimer = Timer.scheduledTimer(timeInterval: 1.0,
                                        target: self,
@@ -158,8 +161,14 @@ class GameScene: SKScene {
                                     transition: transition)
         }else{
             let randomInt = Int.random(in: 0..<5)
-            // モグラに「穴から出たり入ったりするアクション」をつける
-            eggArray[randomInt].run(actionR[randomInt], completion: {self.egg2Array[randomInt].run(self.action2_S[randomInt])})
+            if action_flag[randomInt] == false{
+                // 卵を表示→ひよこ表示
+                action_flag[randomInt] = true
+                egg_nodes[randomInt].run(egg_actions[randomInt], 
+                                         completion: {self.chick_nodes[randomInt].run(self.chick_actions[randomInt],
+                                                                                      completion:{self.action_flag[randomInt] = false})})
+            }
+            
         }
     }
 }
--- TestSwift/Scenes/GameScene/GameSceneTouches.swift
+++ TestSwift/Scenes/GameScene/GameSceneTouches.swift
@@ -15,33 +15,33 @@ extension GameScene {
             // タッチした位置にあるものを調べる
             let touchNode = self.atPoint(location)
             for i in 0...4 {    // 0〜4まで繰り返す
-                // もし、タッチしたものがモグラだったら、やっつける
-                if touchNode == egg2Array[i] {
+                // もし、タッチしたものがひよこだったら、やっつける
+                if touchNode == chick_nodes[i] {
                     // スコアをアップして、表示する
                     score += 10
                     scoreLabel.text = "SCORE:\(score)"
                     
-                    let mog = SKSpriteNode(imageNamed: "mog3.png")  // やられたモグラを作る
+                    let mog = SKSpriteNode(imageNamed: "attachment-3.jpg")  // やられたひよこを作る
                     mog.position = touchNode.position               // 位置を指定する
-                    self.addChild(mog)                              // やられたモグラを表示させる
+                    self.addChild(mog)                              // やられたひよこを表示させる
                     
-                    // 【モグラに回転しながら上に飛び出して消えるアクションをつける】
-                    // モグラを１回転するアクション
+                    // 【ひよこに回転しながら上に飛び出して消えるアクションをつける】
+                    // ひよこを１回転するアクション
                     let action1 = SKAction.rotate(byAngle: 6.28,
                                                   duration: 0.3)
-                    // モグラを上に100移動するアクション
+                    // ひよこを上に100移動するアクション
                     let action2 = SKAction.moveTo(y: touchNode.position.y + 150,
                                                   duration: 0.3)
                     // action1とaction2を同時に行う
                     let actionG = SKAction.group([action1, action2])
-                    // モグラを削除するアクション
+                    // ひよこを削除するアクション
                     let action3 = SKAction.removeFromParent()
                     // actionGとaction3を順番に行う
                     let actionS = SKAction.sequence([actionG, action3])
-                    // モグラに「回転しながら上に飛び出して消えるアクション」をつける
+                    // ひよこに「回転しながら上に飛び出して消えるアクション」をつける
                     mog.run(actionS)
                     
-                    touchNode.position.y = -1000            // モグラを地下（-1000）に移動して消す
+                    touchNode.position.y = -1000            // ひよこを地下（-1000）に移動して消す
                 }
             }
         }
